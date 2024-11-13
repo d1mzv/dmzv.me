577cea1b8d7461a9941a588cde94cfe7be4a051f
d1mzv <dmitry.maznev@gmail.com>
2024-11-13 14:53:25 +0100

    .

--- app/projects/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ProjectsContent } from "@/components/projects-content"
-
-export default function Page() {
-  return <ProjectsContent />
-} 
--- app/youtube/page.tsx
+++
@@ -1,5 +0,0 @@
-import { YoutubeContent } from "@/components/youtube-content"
-
-export default function Page() {
-  return <YoutubeContent />
-} 
