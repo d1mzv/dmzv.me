3ad4d3d12e85f31db6a3c71b0a15a01d5f16e8a8
d1mzv <dmitry.maznev@gmail.com>
2024-11-13 14:05:04 +0100

    Update theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,8 +2,17 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps as NextThemeProviderProps } from "next-themes/dist/types"
 
-export function ThemeProvider({ children, ...props }: NextThemeProviderProps) {
+export function ThemeProvider({ 
+  children, 
+  ...props 
+}: {
+  children: React.ReactNode
+  attribute?: string
+  defaultTheme?: string
+  enableSystem?: boolean
+  storageKey?: string
+  themes?: string[]
+}) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 } 
