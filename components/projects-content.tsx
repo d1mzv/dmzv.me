8ee67ac18e2469daf5c5b63cb11eb9001064ab6a
d1mzv <dmitry.maznev@gmail.com>
2024-11-14 17:27:49 +0100

    Update projects-content.tsx

--- components/projects-content.tsx
+++ components/projects-content.tsx
@@ -104,7 +104,7 @@ export function ProjectsContent() {
               )}
             >
               <div className="relative w-full h-[120px] bg-muted flex items-center justify-center overflow-hidden">
-                <div className="absolute inset-0 bg-gradient-to-b from-transparent from-50% to-background z-10" />
+                <div className="absolute inset-0 bg-gradient-to-b from-transparent from-65% to-background z-10" />
                 <Image
                   src={project.imageUrl}
                   alt={project.name}
