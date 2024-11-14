ca9921dfbfb739fc10edc3a722122fc59a0425c9
d1mzv <dmitry.maznev@gmail.com>
2024-11-14 16:26:10 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { MainNav } from "@/components/main-nav";
-import { ThemeToggle } from "@/components/theme-toggle";
 import { Menu } from "lucide-react";
 import { 
   DropdownMenu,
@@ -48,22 +47,18 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="light"
+          enableSystem={false}
           disableTransitionOnChange
         >
           <Suspense fallback={<div>Loading app...</div>}>
             <div className="flex min-h-screen flex-col">
-              <div className="fixed top-2 right-4 z-50 hidden md:block">
-                <ThemeToggle />
-              </div>
               <header className="sticky top-0 z-40 w-full border-b bg-gray-100 dark:bg-gray-900 backdrop-blur supports-[backdrop-filter]:bg-gray-100/80 dark:supports-[backdrop-filter]:bg-gray-900/80">
                 <div className="container max-w-3xl mx-auto">
                   <div className="flex h-14 items-center">
                     {/* Mobile View */}
                     <div className="md:hidden w-full flex justify-between items-center px-4">
                       <MobileNav />
-                      <ThemeToggle />
                     </div>
                     
                     {/* Desktop View */}
